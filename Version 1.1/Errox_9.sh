107eb6d08661480256a171309b4dd5b23658b8f2
That1EthicalHacker <116861198+vel2006@users.noreply.github.com>
2024-05-12 22:05:47 -0500

    Add files via upload

---
+++ Version 1.1/Errox_9.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#-PRINTING OUT THE CURRENT VERSION OF 'Errox_9', DUE TO ME CHANGING TO JAVA, BUT THIS WILL STAY THE SAME-#
+echo "Version: 1.1"
+#-GETTING THE START TIME, FOR TELLING THE USER HOW LONG THE SCRIPT TAKES-#
+start=$(date +%s)
+#-GETTING THE INTERFACES AVALABLE TO THE USER-#
+echo "Collecting device interfaces"
+#SAVING THE INTERFACES IN A TEMP ARRAY#
+temp=($(ifconfig | grep -E ": flags=" | awk -F '[:]' '{print $1}'))
+#HOLDING USABLE INTERFACES#
+interfaces=()
+#LOOPING THROUGH THE INTERFACES#
+for i in ${!temp[@]}; do
+	#SEEING IF THE INTERFACE CAN BE USED#
+	if [[ "${temp[$i]}" != "lo" ]]; then
+		interfaces+=("${temp[$i]}")
+	fi
+done
+echo "Collected device interfaces: ${interfaces[@]}"
+#-GOING THROUGH THE INTERFACES AND SCANNING THE NETWORK WITH arp-scan TO FIND HOSTS-#
+for i in ${!interfaces[@]}; do
+	echo "Getting network information on ${interfaces[$i]} interface"
+	#GETTING THE USER'S IP#
+	userIP=$(ip a | grep "inet" | grep "brd" | awk '{print $2}' | awk -F '[/]' '{print $1}')
+	#SCANNING FOR DEVICES AND SAVING IT WITHIN A TEMP ARRAY#
+	scanResult=$(sudo arp-scan -I ${interfaces[$i]} --localnet --format='${ip}\t${mac}')
+	#HOLDING FOUND IPs, MACs, AND THE NETWORK TYPE#
+	IPs=()
+	MACs=()
+ 	echo "Getting network speed and type"
+	type=$(echo "$scanResult" | grep -oE 'type:+ [[:alnum:]]+' | awk '{print $2}')
+	#HOLDING THE IPs WITHIN A TEMP ARRAY TO CHECK IF IT'S NEEDED#
+	temp=($(echo "$scanResult" | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print $1}'))
+ 	echo "Getting network devices IPs"
+	#LOOPING THROUGH THE IPs#
+	for j in ${!temp[@]}; do
+		#CHECKING TO SEE IF THE IP IS THE USER OR NETWORK MIN#
+		if [[ $(echo "${temp[$j]}" | awk '{print $1}') != $(echo "$(ip a | grep 'inet' | grep 'brd' | awk '{print $2}' | awk -F '[/]' '{print $1}')") && $(echo "${temp[$j]}" | awk '{print $1}') != $(echo "$(ip a | grep 'inet' | grep 'brd' | awk '{print $2}' | awk -F '[/]' '{print $1}')")  ]]; then
+			#THE IP IS NEITHER, SO IT GETS ADDED#
+			IPs+=("${temp[$j]}")
+		fi
+	done
+	#HOLDING THE MACs WITHIN A TEMP ARRAY TO CHECK IF IT'S NEEDED#
+	temp=($(echo "$scanResult" | grep -oE "([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}"))
+ 	echo "Getting network device MAC addresses"
+	#LOOPING THROUGH THE MACs#
+	for j in ${!temp[@]}; do
+		#CHECKING TO SEE IF THE MAC ADDRESS IS THE USER'S#
+		if [[ $(echo "${temp[$j]}") != $(echo "$(ifconfig ${interfaces[$j]} | grep -oE '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')") ]]; then
+			#IT ISNT AND GETS ADDED#
+			MACs+=("${temp[$j]}")
+		fi
+	done
+	echo "Interface: ${interfaces[$i]}"
+	echo "IPs found: ${IPs[@]}"
+	echo "MACs found: ${MACs[@]}"
+	echo ""
+done
+#-GETTING THE END TIME AND HOW LONG IT TAKES IN SECONDS-#
+end=$(date +%s)
+total=$((end - start))
+echo "It took $((total % 60)) seconds to run the script"
